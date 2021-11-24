464d9c739f2165149845aedf71bff17a2156faf2
Hariharan R <55519209+HariharanR1899@users.noreply.github.com>
2021-11-24 12:32:21 +0530

    Create main.c

---
+++ _implementation/Make file/src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <ctype.h>
+#include <conio.h>
+#include "functions.h"
+int main(){
+	int ch;
+while(1){
+	system("COLOR 5E");
+printf("\n\n\t\t\t*******Welcome to Gaming world*******\n");
+printf("\t\t\t1.ROCK PAPER SCISSORS\n");
+printf("\t\t\t2.GUESS THE NUMBER\n");
+printf("\t\t\t3.EXIT\n");
+printf("\tEnter your choice:");
+scanf("%d",&ch);
+switch(ch){
+	case 1:
+		rps();
+		break;
+	case 2:
+			guessthenumber();
+			break;
+		case 3:
+				exit(0);
+				break;
+		default:
+					printf("Enter a valid choice:");
+				}
+}
